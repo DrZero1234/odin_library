33f359fced0969e12809572357a70c91cd284601
M.Kristóf <drzero123456@gmail.com>
2022-05-25 19:52:17 +0200

    Adding and removing book works

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const LIBRARY_ELEM = document.querySelector(".library")
 const BOOK_FORM_BTN = document.querySelector(".book-form-btn")
 const BOOK_FORM = document.querySelector("#book-form")
 
+
 const BOOK_TITLE = document.querySelector("#book-title")
 const BOOK_AUTHOR = document.querySelector("#book-author")
 const BOOK_YEAR = document.querySelector("#book-year")
 const BOOK_PAGES = document.querySelector("#book-pages")
+const FORM_BUTTON = document.getElementById("submit-book")
+
+document.addEventListener("DOMContentLoaded", () => {
+  const BOOK_ARRAY = LIBRARY_ELEM.querySelectorAll("div")
+})
 
 let form_data = new FormData(BOOK_FORM)
+console.log(form_data)
 
 function Book(title, author, year, pages) {
   this.title = title;
@@ -19,7 +26,7 @@ function Book(title, author, year, pages) {
 }
 
 Book.prototype.addBookToLibrary = function() {
-  myLibrary.push({
+  return myLibrary.push({
     "title": this.title,
     "author": this.author,
     "year": this.year,
@@ -47,15 +54,23 @@ BOOK_FORM.addEventListener("onsubmit", () => {
 */
 
 function displayBooks() {
-  for (const book of myLibrary) {
+  LIBRARY_ELEM.innerHTML = ""
+  for (let book of myLibrary) {
+    // Generating the html card for every book in the library
     let book_div = document.createElement("div");
+    book_div.setAttribute("data-index", myLibrary.indexOf(book))
     book_div.className = "book"
+    // TODObook_div.style.backgroundColor = getRandomColor()
     let remove_button = document.createElement("button")
     remove_button.className = "remove-book"
     remove_button.innerHTML = "Remove"
+    remove_button.addEventListener("click", () => {
+      myLibrary.splice(myLibrary.indexOf(book),1)
+      displayBooks();
+    })
 
-
-    for (const key in book) {
+    // Filling the card with the book data
+    for (let key in book) {
       if (key !== "read") {
         let new_cell = document.createElement("h2");
         if (key === "pages") {
@@ -68,8 +83,6 @@ function displayBooks() {
     }
     book_div.appendChild(remove_button)
     LIBRARY_ELEM.appendChild(book_div);
-
-
   }
   // Displays every book in the library one by one
 
@@ -79,6 +92,24 @@ function displayBooks() {
 
   // Add a button to each book to change its read status. The read status should be stored in the Book contructor
 }
+
+FORM_BUTTON.addEventListener("click", ()=> {
+  new_book = new Book(BOOK_TITLE.value, BOOK_AUTHOR.value,+BOOK_YEAR.value,+BOOK_PAGES.value)
+  new_book.addBookToLibrary();
+  displayBooks();
+})
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+
 
 
 little_prince = new Book("The Little Prince", "Antoine de Saint-Exupéry", 1943, 140)
