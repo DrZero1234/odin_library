bbfb63068d2215f26c4de2d305696cbe74755721
M.Kristóf <drzero123456@gmail.com>
2023-01-20 20:00:24 +0100

    Book add done

--- src/index.js
+++ src/index.js
@@ -13,21 +13,39 @@ const auth = getAuth();
 
 
 
-console.log()
 
 
 // HTML ELEMENTS
+
+const LIBRARY_ELEM = document.querySelector(".library");
+const BOOK_FORM_BTN = document.querySelector(".book-form-btn");
+const BOOK_FORM = document.querySelector("#book-form");
+
+
+
+const BOOK_TITLE = document.querySelector("#book-title");
+const BOOK_AUTHOR = document.querySelector("#book-author");
+const BOOK_YEAR = document.querySelector("#book-year");
+const BOOK_PAGES = document.querySelector("#book-pages");
+const FORM_BUTTON = document.getElementById("submit-book");
+const FORM_CHECK = document.getElementById("book-read");
 
 const header_username = document.getElementById("header-username");
 const singin_btn = document.getElementById("header-signin-btn");
 const signout_btn = document.getElementById("header-signout-btn");
 const header_profile_picture = document.getElementById("header-profile-picture");
 
+
+
+console.log(BOOK_AUTHOR)
+console.log(BOOK_TITLE)
 // Events
 
 singin_btn.addEventListener("click", signIn)
 signout_btn.addEventListener("click", signOutUser)
 header_username.textContent = isUserSignedIn() ? getUserName() : ""
+
+
 
 // AUTH BEGIN
 
@@ -66,6 +84,8 @@ function authStateUi(user) {
   }
 }
 
+
+
  function addSizeToGoogleProfilePic(url) {
    if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
      return url + '?sz=150';
@@ -81,69 +101,73 @@ function getProfilePicture() {
   return getAuth().currentUser.photoURL || "default.png"
 }
 
-
-onAuthStateChanged(auth,function(user) {
-  if (user) {
-    // User is signed in.
-  } else {
-
-    // No user is signed in.
+function checkSignedInWithMessage() {
+  // Return true if the user is signed in Firebase
+  if (isUserSignedIn()) {
+    return true;
   }
-});
+
+  // Display a message to the user using a Toast.
+  var data = {
+    message: 'You must sign-in first',
+    timeout: 2000
+  };
+  return false;
+}
 
 // AUTH END
 
 // FIRESTORE BEGIN
 
-async function saveBook(book_name) {
+async function saveBook(book_title,book_author,book_release_year,book_pages) {
   try {
     await addDoc(collection(getFirestore(), "books"), {
       name: getUserName(),
-      bookName: book_name,
+      bookName: book_title,
+      bookAuthor: book_author,
+      bookReleaseYear: book_release_year,
+      bookPages: book_pages,
+      bookRead: false,
       profilePicUrl: getProfilePicture(),
       timestamp: serverTimestamp()
     });
+    alert("Success")
   }
   catch(error) {
     console.error("Error writing book to the Firebase database", error);
   }
 }
 
+function onBookSumbit(e) {
+  e.preventDefault();
+
+  if (BOOK_TITLE.value && BOOK_AUTHOR.value && BOOK_PAGES.value && BOOK_YEAR.value && checkSignedInWithMessage()) {
+    saveBook(BOOK_TITLE.value, BOOK_AUTHOR.value, BOOK_YEAR.value, BOOK_PAGES.value).then(function () {
+      clearFields()
+    })
+  }
+}
+
 // FIRESTORE END
 
+// UI FUNCTIONS
 
-const LIBRARY_ELEM = document.querySelector(".library");
-const BOOK_FORM_BTN = document.querySelector(".book-form-btn");
-const BOOK_FORM = document.querySelector("#book-form");
+function clearFields() {
+  BOOK_TITLE.value = "";
+  BOOK_AUTHOR.value = "";
+  BOOK_PAGES.value = "";
+  BOOK_YEAR.value = ""
+}
 
-const BOOK_TITLE = document.querySelector("#book-title");
-const BOOK_AUTHOR = document.querySelector("#book-author");
-const BOOK_YEAR = document.querySelector("#book-year");
-const BOOK_PAGES = document.querySelector("#book-pages");
-const FORM_BUTTON = document.getElementById("submit-book");
-const FORM_CHECK = document.getElementById("book-read");
+// UI END //
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const BOOK_ARRAY = LIBRARY_ELEM.querySelectorAll("div");
 });
 
-let form_data = new FormData(BOOK_FORM);
-console.log(form_data);
 
-let Book = class {
-  constructor(title, author, year, pages, read) {
-    this.id = Date.now();
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.pages = pages;
-    this.read = read;
-  }
-
-  addBookToLibrary = () => {
-    localStorage.setItem(this.id, JSON.stringify(this));
-  };
-};
 
 BOOK_FORM_BTN.addEventListener("click", (e) => {
   e.preventDefault();
@@ -154,87 +178,9 @@ BOOK_FORM_BTN.addEventListener("click", (e) => {
   }
 });
 
-function displayBooks() {
-  // Generating the book "cards"
-  LIBRARY_ELEM.innerHTML = "";
-  Object.keys(localStorage)
-    .reverse()
-    .forEach((key) => {
-      const book = JSON.parse(localStorage.getItem(key));
-      console.log(`Book: ${book}`);
-      let book_div = document.createElement("div");
-      book_div.className = "book";
-      book_div.id = book.id;
-      let paper_div = document.createElement("div");
-      paper_div.id = "paper";
-      book_div.appendChild(paper_div)
-
-      let remove_button = document.createElement("button");
-      remove_button.className = "remove-book";
-      remove_button.innerHTML = "Remove";
-      remove_button.addEventListener("click", () => {
-        remove_book(key);
-      });
-
-      Object.keys(book).forEach((book_data) => {
-        if (book_data != "id" && book_data != "read") {
-          let new_cell = document.createElement("h2");
-          if (book_data === "pages") {
-            new_cell.textContent = `${book[book_data]} pages`;
-          } else {
-            new_cell.textContent = book[book_data];
-          }
-          book_div.appendChild(new_cell);
-        } else if (book_data === "read") {
-          let i;
-          let checkbox_label = document.createElement("label");
-          checkbox_label.setAttribute("for", "cbox" + book.id);
-          checkbox_label.textContent = "Read ";
-
-          let checkbox = document.createElement("input");
-          checkbox.setAttribute("type", "checkbox");
-          checkbox.id = "cbox" + book.id;
-
-          // DOES NOT WORK
-
-          checkbox.addEventListener("click", () => {
-            let book_elem = localStorage.getItem(key);
-            let book_json = JSON.parse(book_elem);
-            if (book_json.read) {
-              book_json.read = false;
-            } else {
-              book_json.read = true
-            }
-            localStorage.setItem(key, JSON.stringify(book_json))
-          })
 
 
-          if (book.read === true) {
-            checkbox.checked = true;
-          }
-
-          checkbox_label.appendChild(checkbox);
-          book_div.appendChild(checkbox_label);
-        }
-      });
-
-      book_div.appendChild(remove_button);
-      LIBRARY_ELEM.appendChild(book_div);
-    });
-}
-
-let remove_book = (key) => {
-  if (Object.keys(localStorage).includes(key)) {
-    item = localStorage.getItem(key);
-    localStorage.removeItem(key);
-    displayBooks();
-  } else {
-    alert("This key does not exist");
-    return;
-  }
-};
-
-BOOK_FORM.addEventListener("submit", () => {
+/*BOOK_FORM.addEventListener("submit", () => {
   if (FORM_CHECK.checked) {
     read = true;
   } else {
@@ -251,16 +197,10 @@ BOOK_FORM.addEventListener("submit", () => {
   displayBooks();
 });
 
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+*/
+
+BOOK_FORM.addEventListener("submit", onBookSumbit);
 
 document.addEventListener("DOMContentLoaded", () => {
   initFirebaseAuth();
-  displayBooks();
 })
